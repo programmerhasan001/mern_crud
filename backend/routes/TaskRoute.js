600550c7bdd90737d1b5688f320894cc70dcb624
Hasan <programmermdhasan@gmail.com>
2023-04-05 13:52:42 +0600

    dsj

--- backend/routes/TaskRoute.js
+++ backend/routes/TaskRoute.js
@@ -1,9 +1,12 @@
 const { Router } = require("express");
 
-const { getTasks } = require("../controllers/TaskControllers");
+const { getTasks, saveTask, updateTask, deleteTask } = require("../controllers/TaskControllers");
 
 const router = Router();
 
 router.get("/get", getTasks);
+router.post("/save", saveTask);
+router.put("/update/:id", updateTask);
+router.delete("/delete/:id", deleteTask);
 
 module.exports = router;
